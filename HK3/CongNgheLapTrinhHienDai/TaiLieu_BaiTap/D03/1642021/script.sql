bb4306ef3f48fab6f7c44e7ed552d5d4bc42a2f1
thaihoc215 <32503286+thaihoc215@users.noreply.github.com>
2017-10-18 01:35:53 +0700

    Add database for restful

---
+++ HK3/CongNgheLapTrinhHienDai/TaiLieu_BaiTap/D03/1642021/script.sql
@@ -0,0 +1,52 @@
+﻿CREATE DATABASE REST_ATM
+GO
+USE REST_ATM
+GO
+
+CREATE TABLE The(
+	MaThe int primary key identity(1,1) not null,
+	TenChuThe nvarchar(50) not null,
+	NgayHetHan datetime not null,
+	SoDuKhaDung float not null,
+	NganHang int
+);
+
+Create table NganHang(
+	MaNganHang int primary key not null,
+	TenNganHang nvarchar(50) not null
+);
+
+Create table GiaoDich(
+	MaGiaoDich int,
+	TaiKhoanNguon int,
+	TaiKhoanDich int,
+	ThoiDiemGiaoDich datetime not null,
+	Primary Key (MaGiaoDich,TaiKhoanNguon,TaiKhoanDich)
+);
+
+Alter table The add foreign key (NganHang) references NganHang(MaNganHang);
+Alter table GiaoDich add foreign key (TaiKhoanNguon) references The(MaThe);
+Alter table GiaoDich add foreign key (TaiKhoanDich) references The(MaThe);
+
+--Them ngan hang
+Insert into NganHang(MaNganHang,TenNganHang) values (1,N'VietComBank');
+Insert into NganHang(MaNganHang,TenNganHang) values (2,N'Agribank');
+Insert into NganHang(MaNganHang,TenNganHang) values (3,N'TechComBank');
+Insert into NganHang(MaNganHang,TenNganHang) values (4,N'TienPhongBank');
+Insert into NganHang(MaNganHang,TenNganHang) values (5,N'KienLongBank');
+
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Hà Nguyễn Thái Học', '12/31/2020',10000000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Trần Văn Phương', '12/31/2020',15000000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Nguyễn Kim Ngân', '12/31/2020',11000000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Lê Vũ Anh', '12/31/2020',12040000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Nguyễn Chí Hiếu', '12/31/2020',14000500,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Huỳnh Xuân Phát', '12/31/2020',11040000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Nguyễn Như Ngọc', '12/31/2020',19007000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Trần Quốc Thắng', '12/31/2020',21002000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Nguyễn Hoàng Gia', '12/31/2020',15030000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Vũ Minh Tuấn', '12/31/2020',10000000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Bùi Nhật Trường', '12/31/2020',18500000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Lê Vinh', '12/31/2020',10000000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Nguyễn Hoàng Duy', '12/31/2020',10500000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Vương Kỳ Anh', '12/31/2020',17005000,1);
+Insert into The(TenChuThe,NgayHetHan,SoDuKhaDung,NganHang) values (N'Phạm Hồng Hạnh', '12/31/2020',11001000,1);
---
+++ HK3/PhanTichThietKePhanMem/1.LyThuyet_TaiLieu/BTapThucHanh/W1/1642021/1642021/Model/HocSinhModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1642021.Model
+{
+    public class HocSinhModel
+    {
+        public int MaHS { get; set; }
+        public String TenHS { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public String SDT { get; set; }
+        public String DiaChi { get; set; }
+
+        public HocSinhModel()
+        {
+
+        }
+    }
+}
