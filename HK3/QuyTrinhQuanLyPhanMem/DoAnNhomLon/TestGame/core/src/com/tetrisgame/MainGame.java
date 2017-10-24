c11544384c674ef2e35ccde32d845c2d77cfcbf5
thaihoc215 <32503286+thaihoc215@users.noreply.github.com>
2017-10-25 00:44:54 +0700

    edit project and code demo for tetris game

--- HK3/QuyTrinhQuanLyPhanMem/DoAnNhomLon/TestGame/core/src/com/tetrisgame/MainGame.java
+++ HK3/QuyTrinhQuanLyPhanMem/DoAnNhomLon/TestGame/core/src/com/tetrisgame/MainGame.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
 public class MainGame extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -28,6 +29,6 @@ public class MainGame extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+//		img.dispose();
 	}
 }
