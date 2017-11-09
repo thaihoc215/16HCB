041e689dab62ad3dc34d05566c37bc62e1d4890b
thaihoc1994 <bboyfool215@gmail.com>
2017-11-09 17:42:56 +0700

    BTH4

---
+++ HK3/PhanTichThietKePhanMem/1.LyThuyet_TaiLieu/BTapThucHanh/BT4/1642021_Sprite2D/1642021/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace _1642021
+{
+    partial class frmSprite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.timeMove = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picMain
+            // 
+            this.picMain.Image = global::_1642021.Properties.Resources.Character_Boy;
+            this.picMain.Location = new System.Drawing.Point(175, 145);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(55, 61);
+            this.picMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMain.TabIndex = 0;
+            this.picMain.TabStop = false;
+            // 
+            // timeMove
+            // 
+            this.timeMove.Interval = 10;
+            this.timeMove.Tick += new System.EventHandler(this.timeMove_Tick);
+            // 
+            // frmSprite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 416);
+            this.Controls.Add(this.picMain);
+            this.Name = "frmSprite";
+            this.Text = "SpriteForm";
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.Timer timeMove;
+    }
+}
+
---
+++ HK3/PhanTichThietKePhanMem/1.LyThuyet_TaiLieu/BTapThucHanh/BT4/1642021_Sprite2D/1642021/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1642021
+{
+    
+    public partial class frmSprite : Form
+    {
+        bool _moveUp = false;
+        bool _moveDown = false;
+        bool _moveLeft = false;
+        bool _moveRight = false;
+        bool _isPause = false;
+        int _moveDistance = 5;
+        Point mouseLocation;
+        ContextMenuStrip popupMenu;
+        public frmSprite()
+        {
+            InitializeComponent();
+            this.KeyDown += FrmSprite_KeyDown;
+            this.KeyUp += FrmSprite_KeyUp;
+            this.Load += FrmSprite_Load;
+            picMain.MouseDown += PicMain_MouseDown;
+            picMain.MouseMove += PicMain_MouseMove;
+
+            popupMenu = new ContextMenuStrip();
+            popupMenu.Items.Add("+ : Up speed", null, ItemUpspeedClick);
+            popupMenu.Items.Add("- : Down speed", null, ItemDownSpeedClick);
+            popupMenu.Items.Add("[ : Zoom in", null, ItemZoominClick);
+            popupMenu.Items.Add("] : Zoom out", null, ItemZoomoutClick);
+            popupMenu.Items.Add("S : Pause", null, ItemPauseClick);
+            popupMenu.Items.Add("P : Continue", null, ItemContinueClick);
+            popupMenu.Items.Add("ESC - Close", null, ItemCloseClick );
+        }
+
+        private void FrmSprite_Load(object sender, EventArgs e)
+        {
+            timeMove.Start();
+        }
+
+        private void FrmSprite_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case (int)Keys.Right:
+                    _moveRight = true;
+                    break;
+                case (int)Keys.Left:
+                    _moveLeft = true;
+                    break;
+                case (int)Keys.Up:
+                    _moveUp = true;
+                    break;
+                case (int)Keys.Down:
+                    _moveDown = true;
+                    break;
+                case (int)Keys.S:
+                    _isPause = true;
+                    break;
+                case (int)Keys.P:
+                    _isPause = false;
+                    break;
+                case (int)Keys.Oemplus:
+                case (int)Keys.Add:
+                    _moveDistance++;
+                    break;
+                case (int)Keys.OemMinus:
+                case (int)Keys.Subtract:
+                    if (_moveDistance > 0)
+                        _moveDistance--;
+                    break;
+                case (int)Keys.Escape:
+                    this.Close();
+                    break;
+            }
+        }
+
+        private void FrmSprite_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case (int)Keys.Right:
+                    _moveRight = false;
+                    break;
+                case (int)Keys.Left:
+                    _moveLeft = false;
+                    break;
+                case (int)Keys.Up:
+                    _moveUp = false;
+                    break;
+                case (int)Keys.Down:
+                    _moveDown = false;
+                    break;
+            }
+        }
+
+        private void timeMove_Tick(object sender, EventArgs e)
+        {
+            if (_isPause)
+                return;
+            if (_moveRight)
+                picMain.Location = new Point(picMain.Left + _moveDistance, picMain.Top);
+            if (_moveLeft)
+                picMain.Location = new Point(picMain.Left - _moveDistance, picMain.Top);
+            if (_moveUp)
+                picMain.Location = new Point(picMain.Left, picMain.Top - _moveDistance);
+            if (_moveDown)
+                picMain.Location = new Point(picMain.Left, picMain.Top + _moveDistance);
+            
+        }
+
+        private void PicMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseLocation = e.Location;
+            }
+            if(e.Button == MouseButtons.Right)
+            {
+                popupMenu.Show(Cursor.Position);
+            }
+        }
+
+        private void PicMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                picMain.Left = e.X + picMain.Left - mouseLocation.X;
+                picMain.Top = e.Y + picMain.Top - mouseLocation.Y;
+            }
+        }
+
+        private void ItemContinueClick(object sender, EventArgs e)
+        {
+            _isPause = false;
+        }
+
+        private void ItemPauseClick(object sender, EventArgs e)
+        {
+            _isPause = true;
+        }
+
+        private void ItemZoomoutClick(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void ItemZoominClick(object sender, EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void ItemDownSpeedClick(object sender, EventArgs e)
+        {
+            _moveDistance -= 1;
+        }
+
+        private void ItemUpspeedClick(object sender, EventArgs e)
+        {
+            _moveDistance += 1;
+        }
+        private void ItemCloseClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
