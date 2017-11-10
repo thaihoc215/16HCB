47ffe5d4db32f41033ff744db8350141bb08e563
thaihoc1994 <bboyfool215@gmail.com>
2017-11-10 17:21:36 +0700

    sprite

---
+++ HK3/PhanTichThietKePhanMem/1.LyThuyet_TaiLieu/BTapThucHanh/BT4/1642021_Sprite2D/1642021_2d/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace _1642021_2d
+{
+    partial class frmSprite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timeMove = new System.Windows.Forms.Timer(this.components);
+            this.timeChange = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timeMove
+            // 
+            this.timeMove.Tick += new System.EventHandler(this.timeMove_Tick_1);
+            // 
+            // timeChange
+            // 
+            this.timeChange.Tick += new System.EventHandler(this.timeChange_Tick_1);
+            // 
+            // frmSprite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::_1642021_2d.Properties.Resources.Idle__1_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(109, 109);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimizeBox = false;
+            this.Name = "frmSprite";
+            this.Text = "Sprite2D";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timeMove;
+        private System.Windows.Forms.Timer timeChange;
+    }
+}
+
---
+++ HK3/PhanTichThietKePhanMem/1.LyThuyet_TaiLieu/BTapThucHanh/BT4/1642021_Sprite2D/1642021_2d/Form1.cs
@@ -0,0 +1,273 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1642021_2d
+{
+    public partial class frmSprite : Form
+    {
+        bool _moveUp = false;
+        bool _moveDown = false;
+        bool _moveLeft = false;
+        bool _moveRight = false;
+        bool _isPause = false;
+        int _moveDistance = 5;
+        Point mouseLocation;
+        ContextMenuStrip popupMenu;
+        public frmSprite()
+        {
+            InitializeComponent();
+            this.MinimumSize = new System.Drawing.Size(this.Width, this.Height);
+            this.MaximumSize = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            this.BackColor = Color.LimeGreen;
+            this.TransparencyKey = Color.LimeGreen;
+            this.KeyDown += FrmSprite_KeyDown;
+            this.KeyUp += FrmSprite_KeyUp;
+            this.Load += FrmSprite_Load;
+            //picMain.MouseDown += PicMain_MouseDown;
+            //picMain.MouseMove += PicMain_MouseMove;
+            this.MouseDown += FrmSprite_MouseDown;
+            this.MouseMove += FrmSprite_MouseMove;
+
+            popupMenu = new ContextMenuStrip();
+            popupMenu.Items.Add("+ : Up speed", null, ItemUpspeedClick);
+            popupMenu.Items.Add("- : Down speed", null, ItemDownSpeedClick);
+            popupMenu.Items.Add("[ : Zoom out", null, ItemZoomoutClick);
+            popupMenu.Items.Add("] : Zoom in", null, ItemZoominClick);
+            popupMenu.Items.Add("S : Pause", null, ItemPauseClick);
+            popupMenu.Items.Add("P : Continue", null, ItemContinueClick);
+            popupMenu.Items.Add("B : Edit Sprite", null, ItemChangeSpriteClick);
+            popupMenu.Items.Add("ESC - Close", null, ItemCloseClick);
+        }
+        private void FrmSprite_Load(object sender, EventArgs e)
+        {
+            timeMove.Start();
+            timeMove.Interval = 10;
+            timeChange.Start();
+            timeChange.Interval = 100;
+        }
+
+        private void FrmSprite_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case (int)Keys.Right:
+                    _moveRight = true;
+                    break;
+                case (int)Keys.Left:
+                    _moveLeft = true;
+                    break;
+                case (int)Keys.Up:
+                    _moveUp = true;
+                    break;
+                case (int)Keys.Down:
+                    _moveDown = true;
+                    break;
+                case (int)Keys.S:
+                    _isPause = true;
+                    break;
+                case (int)Keys.P:
+                    _isPause = false;
+                    break;
+                case (int)Keys.Oemplus:
+                case (int)Keys.Add:
+                    _moveDistance += 1;
+                    if (timeChange.Interval > 5)
+                        timeChange.Interval -= 5;
+                    break;
+                case (int)Keys.OemMinus:
+                case (int)Keys.Subtract:
+                    if (_moveDistance > 0)
+                        _moveDistance -= 1;
+                    timeChange.Interval += 5;
+                    break;
+                case (int)Keys.OemOpenBrackets:
+                    this.Size = new Size(this.Width - 10, this.Height - 10);
+                    //picMain.Size = new Size(picMain.Width - 10, picMain.Height - 10);
+                    break;
+                case (int)Keys.OemCloseBrackets:
+                    this.Size = new Size(this.Width + 10, this.Height + 10);
+                    //picMain.Size = new Size(picMain.Width + 10, picMain.Height + 10);
+                    break;
+                case (int)Keys.B:
+                    using (var fbd = new OpenFileDialog())
+                    {
+                        CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+                        dialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                        dialog.IsFolderPicker = true;
+                        if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                        {
+                            MessageBox.Show("You selected: " + dialog.FileName);
+                        }
+                    }
+                    break;
+                case (int)Keys.Escape:
+                    this.Close();
+                    break;
+            }
+        }
+
+        private void FrmSprite_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case (int)Keys.Right:
+                    _moveRight = false;
+                    break;
+                case (int)Keys.Left:
+                    _moveLeft = false;
+                    break;
+                case (int)Keys.Up:
+                    _moveUp = false;
+                    break;
+                case (int)Keys.Down:
+                    _moveDown = false;
+                    break;
+            }
+        }
+        int _attack = 1;
+        int _walk = 1;
+        int _dead = 1;
+        int _ide = 1;
+       
+
+        private void FrmSprite_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left = e.X + this.Left - mouseLocation.X;
+                this.Top = e.Y + this.Top - mouseLocation.Y;
+            }
+        }
+
+        private void FrmSprite_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseLocation = e.Location;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                popupMenu.Show(Cursor.Position);
+            }
+        }
+
+        private void ItemContinueClick(object sender, EventArgs e)
+        {
+            _isPause = false;
+        }
+
+        private void ItemPauseClick(object sender, EventArgs e)
+        {
+            _isPause = true;
+        }
+
+        private void ItemZoomoutClick(object sender, EventArgs e)
+        {
+            this.Size = new Size(this.Width - 10, this.Height - 10);
+            //picMain.Size = new Size(picMain.Width - 10, picMain.Height - 10);
+        }
+
+        private void ItemZoominClick(object sender, EventArgs e)
+        {
+            this.Size = new Size(this.Width + 10, this.Height + 10);
+            //picMain.Size = new Size(picMain.Width + 10, picMain.Height + 10);
+        }
+
+        private void ItemDownSpeedClick(object sender, EventArgs e)
+        {
+            if (_moveDistance > 0)
+                _moveDistance -= 1;
+            timeChange.Interval += 5;
+        }
+
+        private void ItemUpspeedClick(object sender, EventArgs e)
+        {
+            _moveDistance += 1;
+            if (timeChange.Interval > 5)
+                timeChange.Interval -= 5;
+        }
+
+        private void ItemChangeSpriteClick(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                MessageBox.Show("You selected: " + dialog.FileName);
+            }
+        }
+
+        private void ItemCloseClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PicMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if (e.Button == MouseButtons.Left)
+            //{
+            //    picMain.Left = e.X + picMain.Left - mouseLocation.X;
+            //    picMain.Top = e.Y + picMain.Top - mouseLocation.Y;
+            //}
+        }
+        private void PicMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                mouseLocation = e.Location;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                popupMenu.Show(Cursor.Position);
+            }
+        }
+
+        private void timeMove_Tick_1(object sender, EventArgs e)
+        {
+            if (_isPause)
+                return;
+            if (_moveRight)
+                this.Location = new Point(this.Left + _moveDistance, this.Top);
+            //picMain.Location = new Point(picMain.Left + _moveDistance, picMain.Top);
+            if (_moveLeft)
+                this.Location = new Point(this.Left - _moveDistance, this.Top);
+            //picMain.Location = new Point(picMain.Left - _moveDistance, picMain.Top);
+            if (_moveUp)
+                this.Location = new Point(this.Left, this.Top - _moveDistance);
+            //picMain.Location = new Point(picMain.Left, picMain.Top - _moveDistance);
+            if (_moveDown)
+                this.Location = new Point(this.Left, this.Top + _moveDistance);
+            //picMain.Location = new Point(picMain.Left, picMain.Top + _moveDistance);
+        }
+
+        private void timeChange_Tick_1(object sender, EventArgs e)
+        {
+            if (_isPause)
+                return;
+            if (_ide < 16)
+                this.BackgroundImage = Image.FromFile(@"..\..\Resources\male\" + "Idle (" + _ide++ + ")" + ".png");
+            else if (_walk < 11)
+                this.BackgroundImage = Image.FromFile(@"..\..\Resources\male\" + "Walk (" + _walk++ + ")" + ".png");
+            else if (_attack < 9)
+                this.BackgroundImage = Image.FromFile(@"..\..\Resources\male\" + "Attack (" + _attack++ + ")" + ".png");
+            else if (_dead < 13)
+                this.BackgroundImage = Image.FromFile(@"..\..\Resources\male\" + "Dead (" + _dead++ + ")" + ".png");
+            else
+            {
+                _ide = 1;
+                _walk = 1;
+                _attack = 1;
+                _dead = 1;
+            }
+        }
+    }
+}
