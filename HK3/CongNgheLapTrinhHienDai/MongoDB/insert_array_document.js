8a742d4aabf4a59deabed718d41ceb94ac77d483
thaihoc1994 <bboyfool215@gmail.com>
2017-11-14 16:58:54 +0700

    insert mongo

---
+++ HK3/CongNgheLapTrinhHienDai/MongoDB/insert_array_document.js
@@ -0,0 +1,21 @@
+var userArray = [
+    {
+        username : "testinjs1",
+        password : "12345",
+        email : "hochnt@gmail.com",
+        status : "active"
+    },
+    {
+        username : "testinjs2",
+        password : "12345",
+        email : "hochnt@gmail.com",
+        status : "active"
+    },
+    {
+        username : "testinjs3",
+        password : "12345",
+        email : "hochnt@gmail.com",
+        status : "active"
+    }
+];
+db.users.insert(userArray);
---
+++ HK3/CongNgheLapTrinhHienDai/MongoDB/insert_document.js
@@ -0,0 +1,6 @@
+db.users.insert({
+    username : "testinjs",
+    password : "12345",
+    email : "hochnt@gmail.com",
+    status : "active"
+})
