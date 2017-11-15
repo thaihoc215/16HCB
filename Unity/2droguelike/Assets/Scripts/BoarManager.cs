027758e764e9030b04952816a56554456f410db4
thaihoc215 <32503286+thaihoc215@users.noreply.github.com>
2017-11-16 01:40:11 +0700

    show map 2drogue

---
+++ Unity/2droguelike/Assets/Scripts/BoarManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoarManager : MonoBehaviour
+{
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    public int columns = 8;
+    public int rows = 8;
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+
+    public GameObject exit;
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerWallTiles;
+
+    private Transform boardHolder;
+    private List<Vector3> gridPositions = new List<Vector3>();
+
+    void InitialiseList()
+    {
+        gridPositions.Clear();
+        for (int i = 1; i < columns - 1; i++)
+        {
+            for (int j = 1; j < rows - 1; j++)
+            {
+                gridPositions.Add(new Vector3(i, j, 0f));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+        for (int i = -1; i < columns + 1; i++)
+        {
+            for (int j = -1; j < rows + 1; j++)
+            {
+                GameObject toInstantiae = floorTiles[Random.Range(0, floorTiles.Length)];
+                if (i == -1 || i == columns || j == -1 || j == rows)
+                    toInstantiae = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+
+                GameObject instance = Instantiate(toInstantiae, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+
+    }
+
+    //declare iteam random in random postion
+    Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPos = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);
+        return randomPos;
+    }
+
+    void LayoutObjectRandom(GameObject[] tileArray, int minimum, int maximum)
+    {
+        int objectCount = Random.Range(minimum, maximum + 1);
+        for (int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPos = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoice, randomPos, Quaternion.identity);
+        }
+    }
+
+    public void SetupScene(int level)
+    {
+        BoardSetup();
+        InitialiseList();
+        LayoutObjectRandom(wallTiles, wallCount.minimum, wallCount.maximum);
+        LayoutObjectRandom(foodTiles, foodCount.minimum, foodCount.maximum);
+        int enemyCount = (int)Mathf.Log(level, 2f);
+        LayoutObjectRandom(enemyTiles, enemyCount, enemyCount);
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0F), Quaternion.identity);
+    }
+}
---
+++ Unity/2droguelike/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    public BoarManager boarScript;
+    
+    private int level = 3;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boarScript = GetComponent<BoarManager>();
+        InitGame();
+    }
+
+    //init game with the level define in boarmanager
+    void InitGame()
+    {
+        boarScript.SetupScene(level);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
