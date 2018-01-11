1f5a439dabeba792a19929563148c5c59e2a6f40
thaihoc215 <32503286+thaihoc215@users.noreply.github.com>
2018-01-12 01:10:54 +0700

    zend vn reactjs

--- Reactjs/ZendVN/Sec1_ReactJsBasic/ch01/src/App.js
+++ Reactjs/ZendVN/Sec1_ReactJsBasic/ch01/src/App.js
@@ -5,11 +5,38 @@ import './App.css';
 
 class App extends Component {
   render() {
+    //array thong tin khoa hoc
+    const items = [
+      {
+        name: 'React Js',
+        time : '30h',
+        free: false,
+        desc: 'Very Simple!'
+      },
+      {
+        name: 'Angular',
+        time : '55',
+        free: false,
+        desc: ''
+      },
+      {
+        name: 'Node Js',
+        time : '35',
+        free: true,
+        desc: 'Very Simple!'
+      }
+    ];
+
+    const elementCours = items.map((item,index) =>
+      <Course key={index} name = {item.name} time={item.time} free={item.free}>{item.desc}</Course>
+    );
     return (
       <div className="row">
-        <Course />
-        <Course />
-        <Course />
+        {/* <Course /> */}
+        {/* <Course name = "React JS" time="30h" free={true}>Very Simple</Course>
+        <Course name = "Angular" time="55h" free={false}></Course>
+        <Course name = "Node JS" time="35h" free={true}></Course> */}
+        {elementCours}
       </div>
     );
   }
