703c1222379b596a9e2e931b2d5b5f929912d7d4
thaihoc215 <32503286+thaihoc215@users.noreply.github.com>
2018-03-15 02:35:49 +0700

    .

--- javafpt/Lec5_servelet/demo/jspdemo/guest.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE Guests;
-CREATE TABLE Guests (
-       GuestID		INT(5) AUTO_INCREMENT,
-       FirstName	VARCHAR(25) NULL,
-       LastName		VARCHAR(30) NULL,
-       Email		VARCHAR(20) NULL,
-       PRIMARY KEY (GuestID)
-);
